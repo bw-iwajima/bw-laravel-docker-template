6183084b155867bfe3a22455a488911fee3849d8
bw-iwajima <rock.island123@gmail.com>
2025-06-19 00:15:56 +0900

    feat: 新規作成機能に一括代入と脆弱性の対策追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -25,11 +25,12 @@ class TodoController extends Controller
 
     public function store(Request $request)
     {
-        $content = $request->input('content');
+        $inputs = $request->all();
+        dd($inputs);
 
         $todo = new Todo();
 
-        $todo->content = $content;
+        $todo->fill($inputs);
 
         $todo->save();
 
