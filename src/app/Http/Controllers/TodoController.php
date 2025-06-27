4b6a532eb77d4e6d4bee92505e98db9af8d5761e
bw-iwajima <rock.island123@gmail.com>
2025-06-27 10:54:42 +0900

    feat: ToDo詳細表示とToDo編集・更新機能の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -8,10 +8,16 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    private $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }    
+
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        $todos = $this->todo->all();
         
 
         return view('todo.index',['todos'=>$todos]);
@@ -26,13 +32,40 @@ class TodoController extends Controller
     {
         $inputs = $request->all();
 
-        $todo = new Todo();
 
-        $todo->fill($inputs);
+        $this->todo->fill($inputs);
 
-        $todo->save();
+        $this->todo->save();
 
         return redirect()->route('todo.index');
     }
+
+    public function show($id)
+    {
+
+        $todo = $this->todo->find($id);
+
+        return view('todo.show',['todo'=>$todo]);
+
+    }
+
+    public function edit($id)
+    {
+    
+        $todo = $this->todo->find($id);
+        return view('todo.edit',['todo'=>$todo]);
+    }    
+
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->all();
+        
+        $todo = $this->todo->find($id);
+        $todo->fill($inputs)->save();
+
+        return redirect()->route('todo.show',$todo->id);
+
+    }
+
 }
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -22,4 +22,6 @@ Route::get('/todo',function(){
 Route::get('/todo','TodoController@index')->name('todo.index');
 Route::get('/todo/create','TodoController@create')->name('todo.create');
 Route::post('/todo','TodoController@store')->name('todo.store');
-
+Route::get('/todo/{id}','TodoController@show')->name('todo.show');
+Route::get('/todo/{id}/edit','TodoController@edit')->name('todo.edit');
+Route::put('/todo/{id}','TodoController@update')->name('todo.update');
